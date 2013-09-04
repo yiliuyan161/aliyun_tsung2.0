9e89550d1a3562b4a16a98a99a1779fa509d9888
chenjiajia <jiajia.chen@dewmobile.net>
2013-09-04 20:39:19 +0800

    边学变写，视图又写了部分

--- src/main/resources/cn/ben3/ecs/client/view/assets/js/view.js
+++ src/main/resources/cn/ben3/ecs/client/view/assets/js/view.js
@@ -1,5 +1,6 @@
-App.gridView = Ember.View.create({
+App.gridView = Ember.View.extend({
     templateName: 'gridView',
+    checked:true,
     records:[
         {HostName:"a",InstanceId:"b",Status:"run",InnerIpAddress:"100.1.1.1",PublicIpAddress:"1.1.1.1"},
         {HostName:"a",InstanceId:"b",Status:"run",InnerIpAddress:"100.1.1.1",PublicIpAddress:"1.1.1.1"},
@@ -8,9 +9,22 @@ App.gridView = Ember.View.create({
         {HostName:"a",InstanceId:"b",Status:"run",InnerIpAddress:"100.1.1.1",PublicIpAddress:"1.1.1.1"},
         {HostName:"a",InstanceId:"b",Status:"run",InnerIpAddress:"100.1.1.1",PublicIpAddress:"1.1.1.1"}
     ],
-    all:function(){
+    select:[],
+    actions: {
+        all:function(e){
+            if(this.checked){
+                this.set('checked',false);
+            }else{
+                this.set('checked', true);
+            }
+            this.rerender();
+        },
+        one: function(eve) {
+            console.log(eve);
+            this.rerender();
+        }
+    } ,
 
-    },
     checkOne:function(){
 
     },
